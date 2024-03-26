1f78828e65435bb59c654feeae8739f83b50d292
mccartheney <mccartheney@hotmail.com>
2024-03-26 09:56:44 +0000

    Created verify_table_existence function

---
+++ src/verify_table_existence.py
@@ -0,0 +1,11 @@
+# import os to verify if table exists
+import os
+
+def verify_table_existence ( table_name ) :
+    # verify is table exists
+    # True = exists
+    # False = dont exists
+    table_exists = os.path.exists(f"/database/{table_name}")
+
+    # return table existence
+    return table_exists
